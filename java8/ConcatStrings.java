3b31f2696b35c3460f0caed1916c2f8da9b0fb98
Mayank Singh <31184015+mayank-singh-care@users.noreply.github.com>
2025-04-03 14:19:59 +0530

    Update ConcatStrings.java

--- java8/ConcatStrings.java
+++ java8/ConcatStrings.java
@@ -4,7 +4,8 @@ public class ConcatStrings {   // ----------------------
 
 	
 	 public static String concatenateStrings(String str1, String str2) {
-	        return str1.concat(str2);
+	        BiFunction<String, String, String> concatenate = (s1, s2) -> s1 + s2;  //	str1.concat(str2);         
+	         return concatenate.apply(str1, str2);
 	    }
 	 
 	public static void main(String[] args) {
